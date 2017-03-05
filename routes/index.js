8766afff41875cec4ba4a7b39d1e8a66e1a87fa9
Patrik Spathon <patrik@spathon.com>
2017-03-05 22:42:04 +0100

    Add example feed routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,47 @@
 var express = require('express');
 var router = express.Router();
+const fs = require('fs');
+const path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Chamoove API Server - CHAPIS' });
 });
+
+const files = fs.readdirSync(path.resolve(__dirname, '../sample-feed/'));
+for (file of files) {
+  router.get('/' + file, async function(req, res, next) {
+    const stops = await txtToJson(path.resolve(__dirname, '../sample-feed/', file));
+    res.json(stops);
+  });
+}
+
+router.get('/stops', async function(req, res, next) {
+  const stops = await txtToJson(path.resolve(__dirname, '../sample-feed/stops.txt'));
+  res.json(stops);
+});
+
+
+function txtToJson(file) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, 'utf8', (err, file) => {
+      if (err) reject(err);
+
+      const rows = file.split(/\r?\n/);
+      const table = rows.map((r) => r.split(','));
+      const fields = table.shift();
+
+      const items = table.map((columns) => {
+        const item = {};
+        for (let i = 0; i < columns.length; i++) {
+          item[fields[i]] = columns[i];
+        }
+        return item;
+      })
+      resolve(items);
+    });
+  })
+}
+
 
 module.exports = router;
